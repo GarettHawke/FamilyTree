baac80cfc9868ae4dd1c6b6db208c85181e0521f
Peter Vašek <nomad.peter.vasek@gmail.com>
2016-03-15 15:13:14 +0100

    Relation Catalog Test

--- FamilyTree/src/test/java/cz/muni/fi/pb168/familytree/RelationCatalogImplTest.java
+++ FamilyTree/src/test/java/cz/muni/fi/pb168/familytree/RelationCatalogImplTest.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pb168.familytree;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -43,10 +35,6 @@ public class RelationCatalogImplTest {
         manager = new RelationCatalogImpl();
     }
     
-    @After
-    public void tearDown() {
-    }
-
     @Test
     public void makeRelation() {
         manager.makeRelation(person1, person2);
